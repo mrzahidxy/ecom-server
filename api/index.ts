9c80e83c30a83d7c819aeb3fd7e12b80e60bfa2a
Zahid Hasan <mrzahidxy.com>
2024-08-27 17:34:50 +0600

    fix: cors

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import express, { Express} from "express";
 import { PORT } from "../src/secret";
 import rootRouter from "../src/routes";
+import cors from 'cors'
 
 const app: Express = express();
 app.use(express.json());
+app.use(cors({ origin: '*' }))
 
 
 app.use("/api", rootRouter);

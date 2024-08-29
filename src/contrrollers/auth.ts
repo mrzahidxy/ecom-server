ee8c42eccfee4d6bd6b9a07ab3d1d610cb15df32
Zahid Hasan <mrzahidxy.com>
2024-08-29 08:37:39 +0600

    feat: error handlers

--- src/contrrollers/auth.ts
+++ src/contrrollers/auth.ts
@@ -1,9 +1,19 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { compareSync, hashSync } from "bcrypt";
 import jwt from "jsonwebtoken";
 import prisma from "../connect";
+import { BadRequestException } from "../exceptions/bad-request";
+import { ErrorCode } from "../exceptions/root";
+import { UnprocessableEntityException } from "../exceptions/validation";
+import { SignUpSchema } from "../schema/users";
+import { NotFoundException } from "../exceptions/not-found";
 
-export const signup = async (req: Request, res: Response) => {
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  SignUpSchema.parse(req.body);
   const { name, email, password } = req.body;
 
   let user = await prisma.user.findFirst({
@@ -11,8 +21,12 @@ export const signup = async (req: Request, res: Response) => {
   });
 
   if (user) {
-    res.json("User already exists");
-    return;
+    next(
+      new BadRequestException(
+        "User already exists",
+        ErrorCode.USER_ALREADY_EXITS
+      )
+    );
   }
 
   user = await prisma.user.create({
@@ -26,7 +40,11 @@ export const signup = async (req: Request, res: Response) => {
   res.json(user);
 };
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { email, password } = req.body;
 
   let user = await prisma.user.findFirst({
@@ -34,13 +52,15 @@ export const login = async (req: Request, res: Response) => {
   });
 
   if (!user) {
-    res.json("No user found");
-    return;
+    return next(
+      new NotFoundException("No user found", ErrorCode.USER_NOT_FOUND)
+    );
   }
 
   if (!compareSync(password, user.password)) {
-    res.json("Wrong password");
-    return;
+    return next(
+      new BadRequestException("Wrong password", ErrorCode.INCORRECT_PASSWORD)
+    );
   }
 
   const token = jwt.sign({ id: user.id }, "secret", { expiresIn: "1d" });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,3 +1,4 @@
+import { errorHandler } from './../exceptions/error-handler';
 import { Router } from 'express';
 import { login } from './../contrrollers/auth';
 import { signup } from '../contrrollers/auth';
@@ -6,7 +7,7 @@ import { signup } from '../contrrollers/auth';
 
 const authRoutes: Router = Router();
 
-authRoutes.post('/signup', signup)
+authRoutes.post('/signup', errorHandler(signup))
 authRoutes.post('/login', login)
 
 export default authRoutes 
